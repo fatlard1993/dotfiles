31fb85559a6adc6907f1018bcf0ec2d6f1bcfb11
justfatlard <fatlard1993@gmail.com>
2022-04-03 02:08:04 -0700

    update

--- theme/ls.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# Change the colors: https://geoff.greer.fm/lscolors/
-
-export LSCOLORS='exgxxxxxcxxxxxxxxxexex'
-export LS_COLORS='di=34:ln=36:so=0:pi=0:ex=32:bd=0:cd=0:su=0:sg=0:tw=34:ow=34'
