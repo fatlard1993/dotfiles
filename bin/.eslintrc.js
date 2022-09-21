1631d44db6bdf3447bb4cf37f50272f955524373
justfatlard <fatlard1993@gmail.com>
2022-09-20 17:15:47 -0700

    update

--- bin/.eslintrc.js
+++ bin/.eslintrc.js
@@ -1,7 +1,30 @@
 module.exports = {
-	env: {
-		es6: true,
-		node: true
-	},
-	extends: 'eslint:recommended'
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+		'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:json/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  reportUnusedDisableDirectives: true,
+  rules: {
+    /* CS exceptions. */
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'no-underscore-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    'import/order': 'warn',
+    'no-console': 'error',
+    'spaced-comment': 'off',
+    'no-unused-expressions': 'off',
+    'no-return-await': 'off',
+  },
 };
